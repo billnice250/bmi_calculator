7855c3e5e540ca2b6ebe9081868a7922c03fc1ff
Bill Nice <billnicejig@gmail.com>
2021-03-30 21:39:31 +0200

    App v1.0, beta
    Signed-off-by: Bill Nice <billnicejig@gmail.com>

---
+++ lib/calculator_brain.dart
@@ -0,0 +1,44 @@
+import 'dart:math';
+
+import 'package:flutter/cupertino.dart';
+
+class CalculatorBrain {
+  final int height;
+  final int weight;
+  double _bmi;
+
+  CalculatorBrain({@required this.height, @required this.weight}) {
+    this._bmi = weight / pow(height / 100, 2);
+  }
+  String calculateBMI() {
+    return _bmi.toStringAsFixed(1);
+  }
+
+  String getResult() {
+    if (_bmi != null) {
+      if (_bmi >= 25) {
+        return 'Overweight';
+      } else if (_bmi > 18.5) {
+        return 'Normal';
+      } else {
+        return 'Underweight';
+      }
+    } else {
+      return 'BMI not yet calculated';
+    }
+  }
+
+  String getInterpretation() {
+    if (_bmi != null) {
+      if (_bmi >= 25) {
+        return 'You have a higher than normal body weight. Try to exercise more and eat less.';
+      } else if (_bmi > 18.5) {
+        return 'Normal';
+      } else {
+        return 'You have a normal body weight, Good job!';
+      }
+    } else {
+      return 'You have a lower than normal  body weight. You can eat bit more.';
+    }
+  }
+}
--- lib/screens/results_page.dart
+++ lib/screens/results_page.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:my_bmi/calculator_brain.dart';
+import 'package:my_bmi/components/bottom_button.dart';
 import 'package:my_bmi/constants.dart';
 
 import 'file:///C:/Users/billn/AndroidStudioProjects/MyBMI/my_bmi/lib/components/reusable_card.dart';
 
 class ResultsPage extends StatelessWidget {
+  final CalculatorBrain result;
+
+  const ResultsPage({@required this.result});
+
   @override
   Widget build(BuildContext context) {
     SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
@@ -19,19 +25,23 @@ class ResultsPage extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
           Expanded(
-            child: Text(
-              'Your Result',
-              style: kTitleTextStyle,
+            child: Container(
+              padding: EdgeInsets.all(15.0),
+              alignment: Alignment.bottomCenter,
+              child: Text(
+                'Your Result',
+                style: kTitleTextStyle,
+              ),
             ),
           ),
           Expanded(
             flex: 5,
             child: Container(
               margin: const EdgeInsets.symmetric(
-                horizontal: 20.0,
+                horizontal: 10.0,
                 vertical: 0.0,
               ),
-              padding: EdgeInsets.all(0.0),
+              padding: EdgeInsets.all(10.0),
               child: ReusableCard(
                 colour: kActiveCardColor,
                 cardWild: Column(
@@ -39,17 +49,17 @@ class ResultsPage extends StatelessWidget {
                   crossAxisAlignment: CrossAxisAlignment.stretch,
                   children: [
                     Text(
-                      'Normal',
+                      result.getResult(),
                       style: kResultTextStyle,
                       textAlign: TextAlign.center,
                     ),
                     Text(
-                      '20.0',
+                      result.calculateBMI(),
                       style: kBMITextStyle,
                       textAlign: TextAlign.center,
                     ),
                     Text(
-                      'Your BMI is quite low, you should eat more!',
+                      result.getInterpretation(),
                       style: kAdviceMessageTextStyle,
                       textAlign: TextAlign.center,
                     )
@@ -57,6 +67,12 @@ class ResultsPage extends StatelessWidget {
                 ),
               ),
             ),
+          ),
+          BottomButton(
+            onTap: () {
+              Navigator.pop(context);
+            },
+            buttonTitle: 'RE-CALCULATE',
           ),
         ],
       ),
